34afaa0b37518f021594ce7992ff6276540149bf
saySthAbout <saysthabout@gmail.com>
2022-11-29 17:52:24 +0900

    2. JPA 시작하기 2-2 Hello JPA - 애플리케이션 개발
    JPA 구동 방식
    
    실습 - JPA 동작 확인
    - JpaMain 클래스 생성
    - JPA 동작 확인
    
    객체와 테이블을 생성하고 매핑하기
    - @Entity: JPA가 관리할 객체
    - @Id: 데이터베이스 PK와 매핑
    
    ```
    create table Member(
       id bigint not null,
       name varchar(255),
       primary key(id)
    );
    ```
    
    **참고: H2 데이터 베이스 not found 에러시 해결방법 -> https://kmhan.tistory.com/581
    
    - 회원 등록: entityManager.persist(findMember);
    - 회원 수정: findMember.setName("newName"); 하고 따로 persist 처리 안 해줘도 된다. (마치 자바 collection 객체 처럼 다루도록 설계되었기 때문에)
    - 회원 삭제: entityManager.remove(findMember);
    
    [EntityManagerFactory]
    - 이 인스턴스는 프로그램이 시작되면서 디비당 1개만 생성이 된다.
    - 따라서 인스턴스를 하나만 생성해서 애플리케이션 전체에서 공유해야한다.
    
    [EntityManager]
    - 이 인스턴스는 고객의 요청이 올때마다 계속 생성되고 em.close()로 버려지고 한다.
    - 쓰레드간에 공유X (사용하고 버려야 한다.)
    - JPA의 모든 데이터 변경은 트랜잭션 안에서 실행된다.
    
    JPQL 소개
    - 가장 단순한 조회 방법
    	- EntityManager.find()
    	- 객체 그래프 탐색(a.getB().getC())
    - 나이가 18살 이상인 회원을 모두 검색하고 싶다면?
    
    ```
    List<Member> result = em.createQuery("select m from Member as m", Member.class)
                        .setFirstResult(1) //1번 부터
                        .setMaxResults(10) //10개 가져와 => pagination을 각 방언에 맞게 알아서 sql문으로 번역해준다. (persistence.xml에서 설정)
                        .getResultList();
    
                for (Member member : result) {
                    System.out.println("member.name = " + member.getName());
                }
    ```
    
    실습 - JPQL 소개
    - JPQL로 자체 회원 조회하고 싶다.
    
    JPQL
    - JPA를 사용하면 엔티티 객체를 중심으로 개발
    - 문제는 검색 쿼리
    - 검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색
    - 모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능
    - 애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL이 필요
    
    JPA와 JPQL
    - JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
    - SQL과 문법 유사, SELECT, FROM, WHERE, GROUP BY, HAVING, JOIN 지원
    - JPQL은 엔티티 객체를 대상으로 쿼리
    - SQL은 데이터베이스 테이블을 대상으로 쿼리
    
    JPQL
    - 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
    - SQL을 추상화해서 특정 데이터베이스 SQL에 의존X
    - JPQL을 한마디로 정의하면 객체 지향 SQL
    - JPQL은 뒤에서 아주 자세히 다룸

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,14 +15,23 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(3L);
-        member.setName("HelloA");
+        try{
+            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1) //1번 부터
+                    .setMaxResults(10) //10개 가져와 => pagination을 각 방언에 맞게 알아서 sql문으로 번역해준다. (persistence.xml에서 설정)
+                    .getResultList();
 
-        em.persist(member);
-        tx.commit();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
-        em.close();
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close(); // EntityManager가 내부적으로 트랜잭션을 물고 있기 때문에 꼭 닫아줘야 한다.
+        }
         emf.close();
     }
 }
