aa844074944514c03f678be013e87c4d3ff9f3a0
saySthAbout <saysthabout@gmail.com>
2022-11-30 16:52:58 +0900

    4. 엔티티 매핑 4-3 필드와 컬럼 매핑
    요구사항 추가
    ---
    1. 회원은 일반 회원과 관리자로 구분해야 한다.
    2. 회원 가입일과 수정일이 있어야 한다.
    3. 회원을 설명할 수 있는 필드가 있어야 한다. 이 필드는 길이 제한이 없다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-            em.clear();
+            em.persist(member);
 
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("====================");
             tx.commit();
         }catch(Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,40 +1,106 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    @Column(unique = true, length=10)
-    private String name;    
-    private int gogo;
+
+    @Column(name="name", nullable = false)
+    private String username;
+
+    @Column()
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private  Date lastModifiedDate;
+
+    private LocalDate test1;
+    private LocalDateTime test2;
+
+
+    @Lob // 큰 문자열을 담을때 이렇게 하면 clob 자료형이 생성된다.
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Member() { //JPA는 동적으로 뭐 생성하는게 어쩌구 해서 기본 생성자가 1개 있어야 오류가 안난다.
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTemp() {
+        return temp;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
