dd6dabf8d31d49b39643b39c341426766f7d5fab
saySthAbout <saysthabout@gmail.com>
2022-12-03 15:54:53 +0900

    6. 다양한 연관관계 매핑 6-3 일대일 [1:1]
    일대일 관계
    ---
    - 일대일 관계는 그 반대도 일대일
    - 주 테이블이나 대상 테이블 중에 외래 키 선택 가능
    	- 주 테이블에 외래 키
    	- 대상 테이블에 외래 키
    - 외래 키에 데이터베이스 유니크(UNI) 제약조건 추가
    
    일대일: 주 테이블에 외래 키 단방향
    ---
    
    일대일: 주 테이블에 외래 키 단방향 정리
    ---
    - 다대일(@ManyToOne) 단방향 매핑과 유사
    
    일대일: 주 테이블에 외래 키 양방향
    ---
    
    일대일: 주 테이블에 외래 키 양방향 정리
    ---
    - 다대일 양방향 매핑 처럼 외래 키가 있는 곳이 연관관계의 주인
    - 반대편은 mappedBy 적용 (읽기 전용)
    
    일대일: 대상 테이블에 외래 키 단방향
    ---
    
    일대일: 대상 테이블에 외래 키 단방향 정리
    ---
    - 단방향 관계는 JPA 지원X
    - 양방향 관계는 지원
    
    일대일: 대상 테이블에 외래 키 양방향
    ---
    
    일대일: 대상 테이블에 외래 키 양방향
    ---
    - 사실 일대일 주 테이블에 외래 키 양방향과 매핑 방법은 같음
    
    일대일 정리
    ---
    - 주 테이블에 외래 키
    	- 주 객체가 대상 객체의 참조를 가지는 것 처럼 주 테이블에 외래 키를 두고 대상 테이블을 찾음
    	- 객체지향 개발자 선호
    	- JPA 매핑 편리
    	- 장점: 주 테이블만 조회해도 대상 테이블에 데이터가 있는지 확인 가능
    	- 단점: 값이 없으면 외래 키에 null 허용
    - 대상 테이블에 외래 키
    	- 대상 테이블에 외래 키가 존재
    	- 전통적인 데이터베이스 개발자 선호
    	- 장점: 주 테이블과 대상 테이블을 일대일에서 일대다 관계로 변경할 때 테이블 구조 유지
    	- 단점: 프록시 기능의 한계로 지연 로딩으로 설정해도 항상 즉시 로딩됨(프록시는 뒤에서 설명)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -20,7 +20,11 @@ public class Member {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) //매핑은 되고 쓰는데 읽기 전용이 된다.
     private Team team;
-    
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
