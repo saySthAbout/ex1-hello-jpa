bd2aebbd484eada678ec64e8dd0c8aa68f800a62
saySthAbout <saysthabout@gmail.com>
2022-12-07 15:57:26 +0900

    9. 값 타입 9-4 값 타입의 비교
    값 타입의 비교
    ---
    - 값 타입: 인스턴스가 달라도 그 안에 값이 같으면 같은 것으로 봐야 함
    ```
    int a = 10;
    int b = 10;
    ```
    
    ```
    Address a = new Address(“서울시”)
    Address b = new Address(“서울시”)
    ```
    
    값 타입의 비교
    ---
    - 동일성(identity) 비교: 인스턴스의 참조 값을 비교, == 사용
    - 동등성(equivalence) 비교: 인스턴스의 값을 비교, equals() 사용
    - 값 타입은 a.equals(b)를 사용해서 동등성 비교를 해야 함
    - 값 타입의 equals() 메소드를 적절하게 재정의(주로 모든 필드 사용)
    	- intelliJ에서 오버라이드 기능으로 제공해주는 이퀄스를 써라
    	- equals를 오버라이드하면 hashCode() 메서드도 오버라이드 해야지 hash를 사용하는 해쉬맵이라던가 자바 컬렉션에서 사용하는 메서드도 효율적으로 사용할 수 있다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,16 +19,8 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Address address = new Address("city", "street", "10000");
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(address);
-            em.persist(member);
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
-            
             tx.commit();
         }catch(Exception e){
             tx.rollback();
